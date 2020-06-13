edd1f86446de65f342d0e96862d11abe79c6680a
berkeyilmaz <berkeyilmaz003@gmail.com>
2020-06-13 21:00:35 +0300

    Create en.js

---
+++ en.js
@@ -0,0 +1,70 @@
+// General
+const _back_home = " << Return to Home Page";
+
+// Home Page
+const _home_title = "Let's explore Artificial Intelligence";
+const _home_sub = "Access all the resources of the artificial intelligence curriculum for secondary schools where Twin Science content team developed open source with the partnership of UCL-EDUCATE.";
+const _card_why_title = "How Should I Apply Curriculum?";
+const _card_twing_title = "Artificial Intelligence Courses with Twing";
+const _card_twing_sub = "We went over our artificial intelligence content step by step using the Twing app and many other interactive content.";
+const _card_project_form = "Year-End Project Form and Example";
+const _card_project_form_text = "In the year-end project of the curriculum, we asked students to design an interdisciplinary project which they would use artificial intelligence.";
+const _card_interactive_content = "See the List of All Interactive Content";
+const _card_interactive_content_text = "See the list of all online content included in the curriculum.";
+const _card_learning_goals = "Summary of Content and Learning Objectives";
+const _card_learning_goals_text = "Open the Google Sheets document, which contains learning objectives and its links to artificial intelligence.";
+const _card_ml4kids = "Machine Learning with Scratch";
+const _card_ml4kids_content = "'Machine Learning for Kids' project allows students to quickly use machine learning techniques in Scratch projects.";
+const _card_license = "License and Sharing";
+const _card_license_content = "";
+
+const _module_dur = "Recommended Course Duration: {0} hours";
+
+const _module_0_title = "What is Artificial Intelligence?";
+const _module_0_sub = "Artificial intelligence is a science centered around a wide range of interests, with many researchers. Artificial intelligence, which is at the intersection of many different fields such as computer science, mathematics, sociology, linguistics, questions how machines that can think like human be realized. All systems that have natural interaction with human are in the area of interest of artificial intelligence."
+const _module_1_title = "History, Design and Technology: Intelligent Interfaces";
+const _module_1_sub = "The first module of the artificial intelligence (AI) curriculum starts with Intelligent Interface Applications.  We make this start for two reasons: First reason is to establish connections over the interfaces that students create with Scratch or another coding environment, in other words, to strengthen the concepts that they already know.";
+const _module_2_title = "How Computers See: How Do They Make Sense of Pictures?";
+const _module_2_sub = "The second module of the artificial intelligence curriculum starts with computer vision. Computerized vision has different application areas from green screen in cinema to the detection of tumors in medicine. Artificial intelligence has an important place in developing environmental perception. With this activity, we aimed for students to learn about the application areas of artificial intelligence, to develop basic algorithms, to experience various artificial intelligence products and to develop ideas about artificial intelligence for society.";
+const _module_3_title = "How Computers Hear: How Do They Recognize Sounds?";
+const _module_3_sub = "The final module of the AI curriculum aims to show how computers recognize sounds and how they can make sense of sentences. In this module, students learn about different application areas of artificial intelligence, develop basic algorithms in voice recognition, classification and natural language processing, experience various artificial intelligence products and develop ideas about artificial intelligence for society.";
+
+// Why Page
+const _why_title = "Why Did We Developed This Curriculum?";
+const _why_content = "<p>First of all, let's clarify what different industries mean when it comes to artificial intelligence (AI). As a branch of science, artificial intelligence tries to imitate the human way of thinking. For this, it acquires the techniques used in computer science. But now we are quite far from machines that think and act like people. When we say that there is artificial intelligence in a product at the moment, we are actually referring to machine learning which is a sub-branch of artificial intelligence. For example, an AI-powered home combi system creates a learning algorithm in itself by matching the average temperature of the house and the movements of people with each other on an hourly basis.</p>\n" +
+    "<p>For example, in combi, cars, toys, and in many more products different techniques are used for the development of artificial intelligence. As soon as we were born, we started to meet with these products. Our aim in developing this artificial intelligence curriculum is to get students familiar with the answers to questions like how these devices work, what techniques underlie them, how to make it possible to develop these technologies for goodness and humanity.</p>\n" +
+    "<p>This curriculum consists of three modules and a year-end project.</p>\n" +
+    "<h3>How Intelligence Interfaces Are Developed?</h3>\n" +
+    "<p>How can interfaces that students already see on the screen be more intelligent? For example, how does the keyboard application on our mobile phones offer us suggestions? Are the results that Youtube suggests to us ethical? What should we consider if we want to design an interface? How does the process of designing an artificial intelligence-supported product take place?</p>\n" +
+    "<h3>How Computers See?</h3>\n" +
+    "<p>If artificial intelligence is trying to imitate the human, we must first understand our own eyes. How do our eyes see? What is the similarity between the sensors and our eyes? How do computers perceive physical changes and how do they represent numerically? How do we use these numerical values in algorithms? What is the data set? What do we do with collecting datasets? What are the ethical discussions in computer vision?</p>\n" +
+    "<h3>How Computers Hear?</h3>\n" +
+    "<p>How does the human ear work? What are the similarities between a microphone and our ear? How can we classify human voices? How can we promote human language to computers? Can computers make music?</p>\n" +
+    "<h3>Year-End Project: AI for Good</h3>\n" +
+    "<p>Students complete many activities on the implementation of artificial intelligence and technology for 33 weeks. To observe the efficiency of these activities, they develop solutions for one of the United Nations Sustainability Goals with AI.</p>\n" +
+    "<p>After all modules, you may have notice that the modules are following a loop:</p>\n" +
+    "<ol>\n" +
+    "<li>Computers use sensors to detect physical environments.</li>\n" +
+    "<li>Physical changes, with the help of sensors, turn into numerical values that can be used in the algorithm.</li>\n" +
+    "<li>Learning algorithms learn with the help of different data sets.</li>\n" +
+    "<li>Various modalities are required for the interaction of artificial intelligence and human.</li>\n" +
+    "<li>Artificial intelligence can have good and bad consequences for humanity, so ethical issues should be discussed.\n" +
+    "</li>\n" +
+    "</ol>";
+    
+// Twing Youtube Videos Page
+const _twing_title = "Youtube Videos with Twing";
+const _vid_1 = "<h2>History of Computers and Technology</h2>\n" +
+    "<p>The development of artificial intelligence is an important part of the development of computers. Most computer scientists have head toward computer science so that machines can develop intelligence. This history actually reveals the position and possibilities of artificial intelligence right now.<br> In Twing's Prepare for the Future Poster, we asked students to systematically plan their dreams, like scientists who developed artificial intelligence, and create a poster that they can present their dreams to people.<br> In the quiz that helped this event, we got acquainted with these scientists who had dreams. You can reach this quiz that we talked about in the following live YouTube video:</p>\n";
+const _vid_2 = "<h2>Introduction to Artificial Intelligence</h2>\n" +
+    "<p>In this content, you will find the answer to questions such as: What is artificial intelligence? What are the applications of artificial intelligence? Can we easily create artificial intelligence on the web?</p>\n";
+const _vid_3 = "<h2>Computer Vision ~ Green Screen</h2>\n" +
+    "<p>In this section, we will learn different computer vision information from light and color definitions to how the algorithm behind the green screen works.</p>\n";
+const _vid_4 = "<h2>Let's Control the Game with Teachable Machine</h2>\n" +
+    "<p>With Teachable Machine, we were able to write an algorithm that could recognize the images we took from the camera. So in which applications can we use this classifier? Can we use the camera as a control mechanism instead of the keyboard?</p>\n";
+const _vid_5 = "<h2>Quick, Draw! Drawing Data Set</h2>\n" +
+    "<p>Quick, Draw! a game that can quickly detect the shapes we draw. So how can it recognize drawings so quickly?</p>";
+const _vid_6 = "<h2>Google Arts and Culture: Technology and Art</h2>\n" +
+    "<p>With the Arts and Culture project, Google is working hard to make art and culture more accessible to everyone. In addition to this work, Google uses computers and latest generation algorithms as a material of art and experiment!</p>";
+const _vid_7 = "<h2>Let's Work with Math Behind Artificial Intelligence</h2>\n" +
+    "<p>We examined many different projects using artificial intelligence. So which mathematical models are the codes behind these projects based on? Can we work with these mathematical models?</p>\n";
